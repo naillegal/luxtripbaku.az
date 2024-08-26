274c46a382afdedeb3c893155567c2718f86ee8e
naillegal <quluzade.nail@gmail.com>
2024-08-26 12:46:07 +0400

    modeltranslation scripts import changed to https

--- promotion/admin.py
+++ promotion/admin.py
@@ -11,8 +11,8 @@ class CustomTranslationAdmin(TranslationAdmin):
 
     class Media:
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
             'modeltranslation/js/tabbed_translation_fields.js',
         )
         css = {
